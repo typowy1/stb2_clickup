4c57d2aa6726e8fd4ee2de90077905b0c383871f
Rafal Lesnicki <lesnickirafal@gmail.com>
2023-10-11 13:56:51 +0200

    wsio

--- src/main/java/pl/stb2clickup/properties/ClickUpProperties.java
+++ src/main/java/pl/stb2clickup/properties/ClickUpProperties.java
@@ -8,20 +8,28 @@ public class ClickUpProperties {
     private static final String TEAM_ID = "clickup.team.id"; //nazwa klucza
     private static final String SPACE_ENDPOINT = "clickup.space.endpoint"; //nazwa klucza
 
-    public static String getToken(){
-        return getProperty(TOKEN);
+    public static String getToken() {
+        if (getProperty(TOKEN).isEmpty() || getProperty(TOKEN).startsWith("YOUR")) {
+            return System.getProperty("TOKEN");//dodane w jeninsie jako zmienne systemowe
+        } else {
+            return getProperty(TOKEN);
+        }
     }
 
-    public static String getTeamId(){
-        return getProperty(TEAM_ID);
+    public static String getTeamId() {
+        if (getProperty(TEAM_ID).isEmpty() || getProperty(TEAM_ID).startsWith("YOUR")) {
+            return System.getProperty("TEAM_ID"); //dodane w jeninsie jako zmienne systemowe
+        } else {
+            return getProperty(TOKEN);
+        }
     }
 
-    public static String getSpaceEndpoint(){
+    public static String getSpaceEndpoint() {
         return getProperty(SPACE_ENDPOINT);
     }
 
 
-    private static String getProperty(String key){ // zwróci wartość klucza z properties
+    private static String getProperty(String key) { // zwróci wartość klucza z properties
         return ResourceBundle.getBundle("clickup").getString(key);// pierw podajemy nazwe properties, potem nazwe klucza z którego chcemy wyciągnąć wartość
     }
 }
